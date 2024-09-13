f9fa870b26d703a5a4a3dd9f6921f9c076429a04
Sanjosh Satsangi <sanjoshsatsangi@gmail.com>
2024-09-13 12:52:57 +0530

    first commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Card.css';
+
+const Card = ({id, title, tag, status}) => {
+  return (
+    <div className="cardContainer flex-gap-10" style={{gap : '5px'}}>
+        <div className="cardHeading flex-sb">
+            <span style={{textTransform : "uppercase"}} className='color-grey'>{id}</span>
+            <div className="imageContainer relative" style={{ width : "35px", height : "35px"}}>
+                <img style={{width : "100%", height : "100%",  borderRadius : "50%" }}  src="https://images.unsplash.com/photo-1488161628813-04466f872be2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="UserImage" />
+                <div className="showStatus"></div>
+            </div>
+        </div>
+        <div className="cardTitle" style={{fontWeight : 200}} >
+            <p>{title}</p>
+        </div>
+        <div className="cardTags">
+        <div className="tags color-grey"> ... </div>
+            {
+                tag?.map((elem, index) => {
+                    return <div key={index} className="tags color-grey"> <span>•</span> {elem}</div>
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Card;
